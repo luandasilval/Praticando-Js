e77e2380883e92973134a7d572be2fc365e2b84d
luandasilval <luanda.leite@edu.senai.br>
2026-09-14 10:58:02 -0300

    :heavy_plus_sign: Iniciando prompt para sites

--- Praticando js/script.js
+++ Praticando js/script.js
@@ -124,3 +124,176 @@
 
 // let nomes = ["João", "Guilherme", "Maria"];
 // nomes.forEach(nome => console.log(`Seu nome é ${nome}`))
+
+
+//*SOME 
+// VERIFICA se pelo menos um item atende a condição, retorna booleano
+// let numeros = [1,3,5,8]
+
+// let temPar = numeros.some (num => num % 2 === 0 );
+// console.log (temPar) //true
+
+//*EVERY
+// verifica se todos os itens atendem a condição 
+//retorna booleano
+// let temTodosPares = numeros.every (num => num % 2 === 0 );
+// console.log (temTodosPares) //false
+
+// let numeros  = [3, 10, 5, 2, 4]
+// let letras= ["c","a","x","h"]
+
+// //para letras 
+// letras.sort();
+// console.log(letras);
+
+// //para numeros
+// numeros.sort((a, b) => a-b);
+// console.log(numeros)
+
+// let numeros = [1, 2, 3]
+
+// numeros.reverse()
+// console.log(numeros);
+
+// //*JOIN
+// let palavras = ["JS", "é", "top"];
+// let frase = palavras.join (" ");
+// console.log(frase);
+
+//*PUSH
+//*SHIFT
+//*POP
+//*UNSHIFT
+
+// let lista = ["A","B"]
+
+// //PUSH -> ADICIONA elementos no final do array 
+// lista.push ("C");
+// console.log(lista);
+
+// //POP -> remove elementos do final da lista 
+// lista.pop ("C");
+// console.log(lista);
+
+// //SHIFT -> remove do inicio do array
+
+// lista.shift()//remove
+// console.log(lista);
+
+
+// //UNSHIFT -> adiciona do inicio do array
+
+// lista.unshift("A")//adiciona
+// console.log(lista);
+
+
+
+/* 
+array.slice(inicial, final)
+inicial-> onde começa
+final ->onde termina (não inclui o valor da posição)
+
+*/
+//*SLICE
+//cria  uma cópia de uma parte da lista 
+// let numeros = [1, 2, 3, 4]
+
+// let parte = numeros.slice(1, 3) //1 e 3 são as posições do array
+
+// console.log(parte); //2,3
+
+
+
+//*SPLICE
+//remove ou adiciona elementos em qualquero posição
+
+/*
+array.splice (index, count, item1..);
+index->indice -> posição do elemento no array
+count -> numero de elementos removidos (pode ser 0)
+
+*/
+
+//remvovendo sem adc
+// let numeros = [1, 2, 3, 4]
+// numeros.splice(1, 1)
+// console.log(numeros);
+
+
+// //removendo e adicionando
+// let frutas = ["Banana", "Laranja", "Maçã", "Manga"]
+// // frutas.splice(0,3, "Limão", "Kiwi");
+// // console.log(frutas);
+
+
+// frutas.splice(2, 0, "limao", "kiwi");
+// console.log(frutas);
+
+
+// //*REPLACE
+// //substitui uma parte da string 
+// //replace (valorProcurado, ValorQueSubstitui);
+
+// let texto = "Olá mundo!";
+
+// let novoTexto = texto.replace("mundo", "cliente");
+// console.log(novoTexto);
+
+
+
+
+//*NOSSAS FUNÇÕES
+
+// function exibirMensagens() {
+//     console.log("Bem vindo(a)!");
+// }
+
+// * executando a função criada
+// exibirMensagens()
+
+// *função com parametros
+// function cumprimentar(nome) {
+//     console.log(`Olá, ${nome}`);
+// }
+
+// cumprimentar("Ana");
+// cumprimentar("Carlos");
+
+//*função com mais de um parametro
+// const somar = (numero1, numero2) => numero1 + numero2;
+
+
+// console.log (somar(1,2));
+
+// function somarNovo ( numero1, numero2){
+//     console.log(numero1+numero2)
+//return numero1 + numero2
+
+// }
+
+//*PROMPT
+//recebe/solicita informação do usuário
+// let nome = prompt ("Digite o seu nome: ")
+// console.log(nome);
+// alert (nome);
+
+//recebendo um numero e convertendo
+// let idade = Number(prompt("Digite sua idade: "))
+//inteiro
+// idade = parseInt(prompt("Digite sua idade: "))
+
+//float
+// idade = parseFloat(prompt("Digite sua idade: "))
+
+//O que veremos nas proximas aulas
+//spoiler......
+let textoP = document.getElementById("texto");
+
+let saudacao = prompt("Digite a saudação: ")
+
+if (saudacao == "bom dia") {
+    textoP.textContent = saudacao; //textContent insere o conteudo dentro da tag
+}
+else {
+    textoP.textContent = "Segundouuuu ;)";
+}
